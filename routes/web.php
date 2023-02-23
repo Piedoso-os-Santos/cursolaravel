c4c6db4a5d47e4695e3a3bb6a593440337698e07
Piedoso Dos Santos <79217185+Piedoso-os-Santos@users.noreply.github.com>
2023-02-24 01:53:31 +0200

    Insercao de grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/empresa',function(){
-    return view('site/empresa');
-});
+// Route::get('/empresa',function(){
+//     return view('site/empresa');
+// });
 
-// Aprendendo a routa Any, permite todo tipo de acesso
-Route::any('/any',function(){
-    return 'Permite todo tipo de acesso http  (put,delete,get,post)';
-});
+// // Aprendendo a routa Any, permite todo tipo de acesso
+// Route::any('/any',function(){
+//     return 'Permite todo tipo de acesso http  (put,delete,get,post)';
+// });
 
-Route::match(['get','post'],'/match',function(){
-    return 'Permite apenas acessos definidos';
-});
+// Route::match(['get','post'],'/match',function(){
+//     return 'Permite apenas acessos definidos';
+// });
 
-// Adicionando variaveis a url
-Route::get('/produto/{id}/{categoria}',function($id,$categoria){
-    return 'O id do produto eh '.$id.' Categoria: '.$categoria; 
-});
+// // Adicionando variaveis a url
+// Route::get('/produto/{id}/{categoria}',function($id,$categoria){
+//     return 'O id do produto eh '.$id.' Categoria: '.$categoria; 
+// });
 
-// Direcioanmentos
-// Route::redirect('/sobre', '/empresa');
-Route::view('/empresa','site/empresa');
-
-
-Route::get('/news',function(){
-    return view('news');
-})->name('noticias');
+// // Direcioanmentos
+// // Route::redirect('/sobre', '/empresa');
+// Route::view('/empresa','site/empresa');
 
 
-Route::get('/novidades',function(){
-    return redirect()->route('noticias');
-});
+// Route::get('/news',function(){
+//     return view('news');
+// })->name('noticias');
+
+
+// Route::get('/novidades',function(){
+//     return redirect()->route('noticias');
+// });
+
+
