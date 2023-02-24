4e52cf6b4addcb98170f009de9efb8245555bb92
Piedoso Dos Santos <79217185+Piedoso-os-Santos@users.noreply.github.com>
2023-02-24 12:37:53 +0200

    Combinacao de factory e seeder

--- database/factories/CategoriaFactory.php
+++ database/factories/CategoriaFactory.php
@@ -17,7 +17,8 @@ class CategoriaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nome' =>$this->faker->word,
+            'descricao' => $this->faker->text,
         ];
     }
 }
